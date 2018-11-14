5d972c7d192c1168d45256f5151952865910091a
Mike Kramer <mike.kramer@xovis.com>
2018-11-13 19:49:23 -0500

    x
    x

---
+++ SQL_Experiment.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+def my_function():
+
+	conn = sqlite3.connect('SurfSend.db')
+	cursor = conn.cursor()
+	cursor.execute('CREATE TABLE SurfMaster2 AS select SurfReport.ID, SurfReport.SwellSizeFt, SurfReport.SwellIntervalSec, WindInfo.WindMPH, WindDirection.WindDescription, SurfReport.AirTempfrom SurfReport inner join WindInfo on SurfReport.ID = WindInfo.ID inner join WindDirection on WindInfo.ID = WindDirection.ID')
+	conn.commit()
+	cursor.close()
+	conn.close()
+
+my_function()
+
